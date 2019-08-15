b4feef1a297b2890aba2ba19b0888eccc4e477ee
karanjot <karanjot_khatter@hotmail.co.uk>
2019-08-15 17:41:56 +0100

    When confirm button is clicked, data is added into call_data table.

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ order to invest in a new investment.
 - On the new call page you are given a form, with a submit button, you can able to enter a value into this form.
 - Once the button has been submitted, you should see calculations for total notice and total undrawn notice.
 - You should then be prompted with a 'confirm' button, when if clicked it returns to dashboard page.
-- HOWEVER CLICKING THE BUTTON DOES NOT INSERT RECORDS INTO DB. (everything complete except this step.)
+- When you also click the confirm button, a new row is added.. however fund data is not showing.
+- Fund data is not showing as this hasn't been created yet.
 
 # Implementation
 - Tables created and values inserted in mySQL database - called 'validus'. (Navicat)
--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,43 @@
 //header of document - displayed on every page, due to consistency..
 include_once("includes/header.html");
 
-include_once("includes/dashboard.phtml")
+include_once("includes/dashboard.phtml");
+
+if (isset($_POST['cId'])) {
+    insertCallData();
+}
+
+
+function insertCallData() {
+
+    $callID = $_POST['cId'];
+    $capital = $_POST['capital'];
+    $date = $_POST['date'];
+    $investmentName = $_POST['name'];
+
+    $mysqli = new mysqli(HST, USR, PSW, DBN);
+
+    $q = "INSERT INTO data_call (id, call_id, `date`, investment_name, capital_requirement)
+VALUES ('$callID', '$callID', '$date', '$investmentName','$capital')";
+
+        $res = $mysqli->query($q);
+
+        if ($res->num_rows > 0)
+        {
+            //empty array
+            $investment = [];
+
+            while ($row = $res->fetch_assoc()) {
+                //push rows from database into the array
+                $investment[] = $row;
+            }
+        }
+
+        $mysqli->close();
+
+        return $investment;
+
+}
 
 ?>
 
