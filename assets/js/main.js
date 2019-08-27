21c801cadb8103b4709b2f1f9dcd817b1aac9151
karanjot <karanjot_khatter@hotmail.co.uk>
2019-08-27 10:53:01 +0100

    Resolved JS issue on heading navigation between dashboard and call page, by implementing CSS on each page instead.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,33 +9,33 @@ $(document).ready(function() {
         $(callLink).hide();
     }
 
-    $(dashboardLink).click(function () {
-        var clickedLink = $(this);
-        var nonClickedLink = $(".new-call-link");
-        clickedProperties(clickedLink);
-        nonClickedProperties(nonClickedLink);
-    });
-
-    $(callLink).click(function() {
-        // alert('test');
-        var nonClickedLink = $(".dashboard-link");
-        var clickedLink = $(this);
-        clickedProperties(clickedLink);
-        nonClickedProperties(nonClickedLink);
-    });
-
-    function clickedProperties(clickedLink)
-    {
-        $(clickedLink).css('color', 'black');
-        $(clickedLink).css('border-bottom', '3px solid #4472C4');
-        $(clickedLink).css('padding-bottom', '1.4rem');
-        $(clickedLink).prop('disabled', true);
-    }
-
-    function nonClickedProperties(nonClickedLink)
-    {
-        $(nonClickedLink).css('color', 'gray');
-        $(nonClickedLink).css('border-bottom', 'none');
-        $(nonClickedLink).prop('disabled', false);
-    }
+    // $(dashboardLink).click(function () {
+    //     var clickedLink = $(this);
+    //     var nonClickedLink = $(".new-call-link");
+    //     clickedProperties(clickedLink);
+    //     nonClickedProperties(nonClickedLink);
+    // });
+    //
+    // $(callLink).click(function() {
+    //     // alert('test');
+    //     var nonClickedLink = $(".dashboard-link");
+    //     var clickedLink = $(this);
+    //     clickedProperties(clickedLink);
+    //     nonClickedProperties(nonClickedLink);
+    // });
+    //
+    // function clickedProperties(clickedLink)
+    // {
+    //     $(clickedLink).css('color', 'black');
+    //     $(clickedLink).css('border-bottom', '3px solid #4472C4');
+    //     $(clickedLink).css('padding-bottom', '1.4rem');
+    //     $(clickedLink).prop('disabled', true);
+    // }
+    //
+    // function nonClickedProperties(nonClickedLink)
+    // {
+    //     $(nonClickedLink).css('color', 'gray');
+    //     $(nonClickedLink).css('border-bottom', 'none');
+    //     $(nonClickedLink).prop('disabled', false);
+    // }
 });
